f0df0ee6f5925ec1e9a9f71805f956706986f073
David Park <kjparkdavid@users.noreply.github.com>
2024-08-20 13:22:54 -0400

    Update miuuShopSection.tsx

--- components/miuuShopSection.tsx
+++ components/miuuShopSection.tsx
@@ -4,17 +4,16 @@ import miyomiRibbon from "@/public/images/miuuShop/miyomiRibbon.gif";
 const MiuuShopSection = () => {
   return (
     <div className="bg-smoothRed px-6 pt-9 pb-16 text-center items-center flex flex-col">
-      <h3 className="font-bold text-xl">Miuu Shop is Coming Soon!</h3>
+      <h3 className="font-bold text-xl">Visit our Miuu Studio Store!</h3>
       <Image
         src={miyomiRibbon}
         alt={"miyomiRibbon"}
         width={129}
         className="mt-6 mb-8"
       />
-      <p className="text-sm">
-        We are preparing official Miuu Studio shop. <br></br>Be the first to
-        know when we launch!
-      </p>
+      <a className="text-md" href="https://miuustudio.com/" target="_blank">
+        Visit Miuu Studio Store
+      </a>
     </div>
   );
 };
