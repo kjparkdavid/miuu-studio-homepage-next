5370ca036482997819c3f24e04a84bc924e33a61
David Park <kjparkdavid@users.noreply.github.com>
2024-12-17 23:15:42 -0500

    Update index.tsx

--- pages/privacy-policy/index.tsx
+++ pages/privacy-policy/index.tsx
@@ -13,163 +13,139 @@ const PrivacyPolicyPage = () => {
       </Head>
 
       <main
-        className={`flex min-h-screen flex-col justify-between ${montserrat.className} p-6`}
-      >
-        <strong>Privacy Policy</strong>
-        <p>
-          Miuu Studio built the Miuu Note - Diary With Lock app as a Free app.
-          This SERVICE is provided by Miuu Studio at no cost and is intended for
-          use as is.
-        </p>
-        <p>
-          This page is used to inform visitors regarding my policies with the
-          collection, use, and disclosure of Personal Information if anyone
-          decided to use my Service.
-        </p>
-        <p>
-          If you choose to use my Service, then you agree to the collection and
-          use of information in relation to this policy. The Personal
-          Information that I collect is used for providing and improving the
-          Service. I will not use or share your information with anyone except
-          as described in this Privacy Policy.
-        </p>
-        <p>
-          The terms used in this Privacy Policy have the same meanings as in our
-          Terms and Conditions, which are accessible at Miu Note - Diary With
-          Lock unless otherwise defined in this Privacy Policy.
-        </p>
-        <p>
-          <strong>Information Collection and Use</strong>
-        </p>
-        <p>
-          For a better experience, while using our Service, I may require you to
-          provide us with certain personally identifiable information. The
-          information that I request will be retained on your device and is not
-          collected by me in any way.
-        </p>
-        <div>
-          <p>
-            The app does use third-party services that may collect information
-            used to identify you.
-          </p>
-          <p>
-            Link to the privacy policy of third-party service providers used by
-            the app
-          </p>
-          <ul>
-            <li>
-              <a
-                href="https://www.google.com/policies/privacy/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Google Play Services
-              </a>
-            </li>
-          </ul>
-        </div>
-        <p>
-          <strong>Log Data</strong>
-        </p>
-        <p>
-          I want to inform you that whenever you use my Service, in a case of an
-          error in the app I collect data and information (through third-party
-          products) on your phone called Log Data. This Log Data may include
-          information such as your device Internet Protocol (“IP”) address,
-          device name, operating system version, the configuration of the app
-          when utilizing my Service, the time and date of your use of the
-          Service, and other statistics. However, it will never collect any user
-          typed input whatsoever.
-        </p>
-        <p>
-          <strong>Cookies</strong>
-        </p>
-        <p>
-          Cookies are files with a small amount of data that are commonly used
-          as anonymous unique identifiers. These are sent to your browser from
-          the websites that you visit and are stored on your device&apos;s
-          internal memory.
-        </p>
-        <p>
-          This Service does not use these “cookies” explicitly. However, the app
-          may use third-party code and libraries that use “cookies” to collect
-          information and improve their services. You have the option to either
-          accept or refuse these cookies and know when a cookie is being sent to
-          your device. If you choose to refuse our cookies, you may not be able
-          to use some portions of this Service.
-        </p>
-        <p>
-          <strong>Service Providers</strong>
-        </p>
-        <p>
-          I may employ third-party companies and individuals due to the
-          following reasons:
-        </p>
-        <ul>
-          <li>To facilitate our Service;</li>
-          <li>To provide the Service on our behalf;</li>
-          <li>To perform Service-related services; or</li>
-          <li>To assist us in analyzing how our Service is used.</li>
-        </ul>
-        <p>
-          I want to inform users of this Service that these third parties have
-          access to their Personal Information. The reason is to perform the
-          tasks assigned to them on our behalf. However, they are obligated not
-          to disclose or use the information for any other purpose.
-        </p>
-        <p>
-          <strong>Security</strong>
-        </p>
-        <p>
-          I value your trust in providing us your Personal Information, thus we
-          are striving to use commercially acceptable means of protecting it.
-          But remember that no method of transmission over the internet, or
-          method of electronic storage is 100% secure and reliable, and I cannot
-          guarantee its absolute security.
-        </p>
-        <p>
-          <strong>Links to Other Sites</strong>
-        </p>
-        <p>
-          This Service may contain links to other sites. If you click on a
-          third-party link, you will be directed to that site. Note that these
-          external sites are not operated by me. Therefore, I strongly advise
-          you to review the Privacy Policy of these websites. I have no control
-          over and assume no responsibility for the content, privacy policies,
-          or practices of any third-party sites or services.
-        </p>
-        <p>
-          <strong>Children&apos;s Privacy</strong>
-        </p>
-        <div>
-          <p>
-            These Services do not address anyone under the age of 13. I do not
-            knowingly collect personally identifiable information from children
-            under 13 years of age. In the case I discover that a child under 13
-            has provided me with personal information, I immediately delete this
-            from our servers. If you are a parent or guardian and you are aware
-            that your child has provided us with personal information, please
-            contact me so that I will be able to do the necessary actions.
-          </p>
-        </div>
-        <p>
-          <strong>Changes to This Privacy Policy</strong>
-        </p>
-        <p>
-          I may update our Privacy Policy from time to time. Thus, you are
-          advised to review this page periodically for any changes. I will
-          notify you of any changes by posting the new Privacy Policy on this
-          page.
-        </p>
-        <p>This policy is effective as of 2022-07-01</p>
-        <p>
-          <strong>Contact Us</strong>
-        </p>
-        <p>
-          If you have any questions or suggestions about my Privacy Policy, do
-          not hesitate to contact me at support@miuustudio.com.
-        </p>
-      </main>
+  className={`flex min-h-screen flex-col justify-between ${montserrat.className} p-6`}
+>
+  <strong>Privacy Policy</strong>
+  <p>
+    Miuu Studio created the Miuu Note - Diary With Lock app as a free application
+    with optional in-app purchases and ads. The base version of this Service is
+    provided at no cost and is intended for use as-is. Users can opt to purchase
+    a subscription or a one-time permanent upgrade for an ad-free experience and
+    access to premium features.
+  </p>
+  <p>
+    This Privacy Policy informs users about how their Personal Information is
+    collected, used, and disclosed when using this Service. By using the Service,
+    you agree to the collection and use of information as described in this
+    policy.
+  </p>
+  <p>
+    <strong>Information Collection and Use</strong>
+  </p>
+  <p>
+    The app may request personally identifiable information for a better user
+    experience. This information is stored on your device and is not collected
+    by Miuu Studio.
+  </p>
+  <p>
+    However, the app uses third-party services that may collect data to identify
+    you. These include:
+  </p>
+  <ul>
+    <li>Google Play Services</li>
+  </ul>
+  <p>
+    <strong>Data Backup and Storage</strong>
+  </p>
+  <p>
+    <strong>Apple Login Users</strong>: For users who sign in with Apple Login, we
+    use Google Cloud Storage to securely back up user-input data (such as diary
+    entries). This ensures your data is recoverable if you switch devices or
+    reinstall the app. Miuu Studio retains management control of the data stored
+    on Google Cloud but does not directly host or access it. For more
+    information, please review{" "}
+    <a
+      href="https://cloud.google.com/privacy"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Google Cloud’s Privacy Policy
+    </a>
+    .
+  </p>
+  <p>
+    <strong>Google Login Users</strong>: For users who sign in with Google Login,
+    user-input data is backed up directly to your Google Drive. This means no
+    data is sent to or stored by Miuu Studio. You retain full ownership and
+    control of the data stored in your Google Drive. Please refer to{" "}
+    <a
+      href="https://policies.google.com/privacy"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Google Drive’s Privacy Policy
+    </a>{" "}
+    for details on their data handling practices.
+  </p>
+  <p>
+    <strong>Log Data</strong>
+  </p>
+  <p>
+    If the app encounters an error, it collects data such as your device's IP
+    address, name, operating system, and usage statistics through third-party
+    services. No user-input data is ever collected.
+  </p>
+  <p>
+    <strong>Cookies</strong>
+  </p>
+  <p>
+    The app itself does not use cookies but may include third-party code or
+    libraries that do. You can choose to accept or refuse cookies in your
+    settings, but some features may be affected.
+  </p>
+  <p>
+    <strong>Third-Party Service Providers</strong>
+  </p>
+  <ul>
+    <li>Facilitate the Service</li>
+    <li>Perform Service-related functions</li>
+    <li>Analyze Service usage</li>
+  </ul>
+  <p>
+    These providers may access your Personal Information but are obligated not to
+    misuse it.
+  </p>
+  <p>
+    <strong>Security</strong>
+  </p>
+  <p>
+    We take reasonable measures to protect your Personal Information but cannot
+    guarantee absolute security due to the nature of internet transmission and
+    electronic storage.
+  </p>
+  <p>
+    <strong>Links to Other Sites</strong>
+  </p>
+  <p>
+    The app may contain links to external websites. Miuu Studio is not
+    responsible for the privacy practices or content of these sites. Review their
+    Privacy Policies for more information.
+  </p>
+  <p>
+    <strong>Children’s Privacy</strong>
+  </p>
+  <p>
+    This Service is not intended for children under 13. We do not knowingly
+    collect data from children under 13. If you believe your child has provided
+    Personal Information, please contact us for prompt deletion.
+  </p>
+  <p>
+    <strong>Changes to This Policy</strong>
+  </p>
+  <p>
+    This Privacy Policy may be updated occasionally. Users are encouraged to
+    review this page regularly. Changes will be posted here and are effective
+    immediately.
+  </p>
+  <p>Effective Date: July 1, 2022</p>
+  <p>
+    <strong>Contact Us</strong>
+  </p>
+  <p>
+    For questions or suggestions, contact us at:{" "}
+    <a href="mailto:support@miuustudio.com">support@miuustudio.com</a>
+  </p>
+</main>
     </>
   );
 };
